1b67ebc0e5b0e44779b21436d745383d3e82304f
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2023-12-21 11:37:22 +0000

    File5Parte2

--- first-rest-api.py
+++ first-rest-api.py
@@ -25,3 +25,53 @@ def getAllData():
         print(x);
         result.append(x);
     return result
+
+
+@app.route("/air_transport")
+def airTransport():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Transport='Air'")
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+@app.route("/epic_units")
+def epicUnits():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Rarity = 'Epic'")
+    myresult = mycursor.fetchall()
+    result = [];
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+@app.route("/speed")
+def speed():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Speed = 'Fast (90)'")
+    myresult = mycursor.fetchall()
+    result = [];
+
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+
+
+@app.route("/hit_speed")
+def hit_speed():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Hit_Speed = '1.2 sec'")
+    myresult = mycursor.fetchall()
+    result = [];
+
+    for x in myresult:
+        print(x);
+        result.append(x);
+    return result
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
